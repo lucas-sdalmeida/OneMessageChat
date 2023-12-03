a688c393c9af194320a224e80446a0e46aa73f92
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-12-02 23:27:49 -0300

    refactor: removed ExampleTest and moved MainActivity to view package

--- app/src/main/java/com/lucassdalmeida/onemessagechat/view/MainActivity.kt
+++ app/src/main/java/com/lucassdalmeida/onemessagechat/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lucassdalmeida.onemessagechat
+package com.lucassdalmeida.onemessagechat.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucassdalmeida.onemessagechat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
