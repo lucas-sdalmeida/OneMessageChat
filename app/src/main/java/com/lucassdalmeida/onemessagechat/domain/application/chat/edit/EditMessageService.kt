9a51765c645f7b3f2e4b6026a46c1baabf02c0d2
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-12-03 13:07:27 -0300

    feat: implemented EditMessageService

---
+++ app/src/main/java/com/lucassdalmeida/onemessagechat/domain/application/chat/edit/EditMessageService.kt
@@ -0,0 +1,15 @@
+package com.lucassdalmeida.onemessagechat.domain.application.chat.edit
+
+import com.lucassdalmeida.onemessagechat.domain.application.chat.repository.ChatRepository
+
+class EditMessageService(
+    private val chatRepository: ChatRepository,
+) {
+    fun edit(id: String, message: String) {
+        val chat = chatRepository.findById(id) ?:
+            throw NoSuchElementException("There is not a chat with id: $id")
+        
+        chat.message = message
+        chatRepository.create(chat)
+    }
+}
